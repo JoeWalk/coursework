808f787dbf1f602f11c21da03320aa1bcf301394
JW211882 <joewalker2005@icloud.com>
2022-06-29 12:47:05 +0100

    Register and log in done

--- src/com/company/Admin.java
+++ src/com/company/Admin.java
@@ -1,20 +1,23 @@
 package com.company;
 
+import java.sql.*;
+
 public class Admin {
 
     public static String getEmail(String email) {
         boolean valid = false;
-        while(!valid){
+        while (!valid) {
             email = Main.getInput("Please enter the email you would like to use for your account");
             if (email.contains("@") && email.contains(".")) {
                 valid = true;
-            }
-            else{
+            } else {
                 System.out.println("Please enter a valid email");
             }
         }
-        return(email);
+
+        return (email);
     }
+
 
     public static String getPassword(String password) {
         boolean valid = false;
@@ -28,5 +31,62 @@ public class Admin {
             }
         }
         return(password);
+    }
+
+    public static void writeToDatabase(String email, String password) {
+        String DatabaseLocation = "jdbc:ucanaccess://X://My Documents//Computer Science//Coursework//database1.accdb";
+
+        try (Connection con = DriverManager.getConnection(DatabaseLocation)) {
+
+            Statement stmt = con.createStatement(ResultSet.TYPE_SCROLL_INSENSITIVE, ResultSet.CONCUR_UPDATABLE);
+
+            String sql = "INSERT INTO LogIn (Email, Password, Balance) VALUES (?, ?, ?)";
+
+            PreparedStatement preparedStatement = con.prepareStatement(sql);
+            preparedStatement.setString(1, email);
+            preparedStatement.setString(2, password);
+            preparedStatement.setInt(3, 100);
+
+            int row = preparedStatement.executeUpdate();
+        }
+        catch (Exception e) {
+            System.out.println("Error in the SQL class: " + e);
+        }
+    }
+
+    public static void showBalance (String email) {
+        String DatabaseLocation = "jdbc:ucanaccess://X://My Documents//Computer Science//Coursework//database1.accdb";
+
+        try (Connection con = DriverManager.getConnection(DatabaseLocation)) {
+
+            Statement stmt = con.createStatement(ResultSet.TYPE_SCROLL_INSENSITIVE, ResultSet.CONCUR_UPDATABLE);
+
+            String sql = "SELECT * FROM LogIn";
+
+            ResultSet rs = stmt.executeQuery(sql);
+
+            while(rs.next()) {
+                if (email.equals(rs.getString("Email"))) {
+                    System.out.println("£" + rs.getString("Balance"));
+                }
+            }
+        }
+        catch (Exception e) {
+            System.out.println("Error in the SQL class: " + e);
+        }
+    }
+
+    public static void displayMainMenu() {
+        String[] circuits = {"Bahrain","Saudi Arabia", "Australia", "Imola", "Miami", "Spain", "Monaco", "Azerbaijan", "Canada", "Britain", "Austria", "France", "Hungary",
+        "Belgium", "Netherlands", "Italy", "Singapore", "Japan", "USA", "Mexico", "Sao Paulo", "Abu Dhabi"};
+        int currentCircuit = 0;
+        System.out.println("A) Start next race in " + circuits[currentCircuit]);
+        System.out.println("B) Simple explanation of Formula 1");
+        System.out.println("C) Explanation of betting system");
+        System.out.println("Q) Quit");
+    }
+
+    public static void preQualifyingBets() {
+
     }
 }
--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,19 +1,98 @@
 package com.company;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.Statement;
 import java.util.Scanner;
 
 public class Main {
 
     public static void main(String[] args) {
-	    String email = "";
-        email = Admin.getEmail(email);
+        boolean logInSuccessful = false;
+        String email = "";
         String password = "";
-        password = Admin.getPassword(password);
+        while (logInSuccessful == false) {
+            String answer = getInput("Do want to log in (L) or register for an account (R)? ");
+            if (answer.equals("L")){
+                email = Admin.getEmail(email);
+                password = Admin.getPassword(password);
+                logInSuccessful = logIn(email, password);
+                if (logInSuccessful == false) {
+                    System.out.println("Not a  registered email or password");
+                }
+            }
+            else {
+                email = Admin.getEmail(email);
+                password = Admin.getPassword(password);
+                Admin.writeToDatabase(email,password);
+            }
+        }
+        Admin.showBalance(email);
+
+        Admin.displayMainMenu();
+
+        Character ans = ' ';
+        while (ans != 'Q') {
+            if (ans == 'A') {
+                Admin.preQualifyingBets();
+            }
+        }
+
     }
 
     public static String getInput(String prompt) {
         System.out.println(prompt);
         Scanner input = new Scanner(System.in);
         return input.nextLine();
+    }
+
+    public static boolean logIn(String email, String password) {
+        boolean valid = false;
+        boolean emailValid = false;
+        boolean passwordValid = false;
+
+        String DatabaseLocation = "jdbc:ucanaccess://X://My Documents//Computer Science//Coursework//database1.accdb";
+
+        try (Connection con = DriverManager.getConnection(DatabaseLocation)) {
+
+            Statement stmt = con.createStatement(ResultSet.TYPE_SCROLL_INSENSITIVE, ResultSet.CONCUR_UPDATABLE);
+
+            String sql = "SELECT * FROM LogIn";
+
+            ResultSet rs = stmt.executeQuery(sql);
+
+            while(rs.next()) {
+                if (email.equals(rs.getString("Email"))) {
+                    emailValid = true;
+                }
+            }
+        }
+        catch (Exception e) {
+            System.out.println("Error in the SQL class: " + e);
+        }
+
+        try (Connection con = DriverManager.getConnection(DatabaseLocation)) {
+
+            Statement stmt = con.createStatement(ResultSet.TYPE_SCROLL_INSENSITIVE, ResultSet.CONCUR_UPDATABLE);
+
+            String sql = "SELECT * FROM LogIn";
+
+            ResultSet rs = stmt.executeQuery(sql);
+
+            while(rs.next()) {
+                if (password.equals(rs.getString("Password"))) {
+                    passwordValid = true;
+                }
+            }
+        }
+        catch (Exception e) {
+            System.out.println("Error in the SQL class: " + e);
+        }
+
+        if ((emailValid == true) && (passwordValid == true)) {
+            valid = true;
+        }
+        return valid;
     }
 }
